a4a8757235b0cf8a5a68bb9071b4c055747f5111
LKolinko <lskolinko@gmail.com>
2025-05-13 11:59:15 +0300

    variadic template index

--- tuple.hpp
+++ tuple.hpp
@@ -31,21 +31,6 @@ namespace TupleItems {
     };
 }
 
-namespace GetTypes {
-    template <size_t ind, typename... Tail>
-    struct impl;
-
-    template <size_t ind, typename T, typename... Tail>
-    struct impl<ind, T, Tail...> {
-        using Type = typename impl<ind - 1, Tail...>::Type;
-    };
-
-    template <typename T,  typename... Tail>
-    struct impl<0, T, Tail...> {
-        using Type = T;
-    };
-}
-
 template <typename... Types>
 struct Tuple : TupleItems::TupleSet<0, Types...> {
     using Base = TupleItems::TupleSet<0, Types...>;
@@ -58,5 +43,5 @@ struct Tuple : TupleItems::TupleSet<0, Types...> {
 
 template<size_t ind, typename... Types> requires(ind < sizeof...(Types))
 decltype(auto) get(Tuple<Types...>& t) {
-    return static_cast<TupleItems::TupleUnit<ind, typename GetTypes::impl<ind, Types...>::Type>&>(t).get();
+    return static_cast<TupleItems::TupleUnit<ind, Types...[ind]>&>(t).get();
 }
