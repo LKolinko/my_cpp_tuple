a3bb271a63dc338599a6e7b3e35d68465c79849d
LKolinko <lskolinko@gmail.com>
2025-05-13 09:37:34 +0300

    tuple.hpp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,8 @@
-#include <cstddef>
-#include <memory>
-#include <utility>
-#include <iostream>
 #include <tuple>
+#include "tuple.hpp"
 #include <string>
+#include <iostream>
 
-namespace TupleItems {
-    template <size_t ind, typename T>
-    struct TupleUnit {
-        T item;
-
-        constexpr TupleUnit() = default;
-        constexpr TupleUnit(T&& item) : item(item) {}
-
-        constexpr T& get() {
-            return item;
-        }
-    };
-
-    template <size_t ind, typename... Tail>
-    struct TupleSet;
-
-    template <size_t ind>
-    struct TupleSet<ind> {};
-
-    template <size_t ind, typename T, typename... Tail>
-    struct TupleSet<ind, T, Tail...> : TupleUnit<ind, T>, TupleSet<ind + 1, Tail...> {
-        constexpr TupleSet() = default;
-        
-        template <typename Arg, typename... ArgTail>
-        constexpr TupleSet(Arg&& arg, ArgTail&&... tail) : TupleUnit<ind, T>(std::forward<Arg>(arg)), 
-        TupleSet<ind + 1, Tail...>(std::forward<ArgTail>(tail)...) {}
-    };
-}
-
-namespace GetTypes {
-    template <size_t ind, typename... Tail>
-    struct impl;
-
-    template <size_t ind, typename T, typename... Tail>
-    struct impl<ind, T, Tail...> {
-        using Type = typename impl<ind - 1, Tail...>::Type;
-    };
-
-    template <typename T,  typename... Tail>
-    struct impl<0, T, Tail...> {
-        using Type = T;
-    };
-}
-
-template <typename... Types>
-struct Tuple : TupleItems::TupleSet<0, Types...> {
-    using Base = TupleItems::TupleSet<0, Types...>;
-    
-    constexpr Tuple() = default;
-
-    template <typename... Args>
-    constexpr Tuple(Args&&... args) : Base(std::forward<Args>(args)...) {}
-};
-
-template<size_t ind, typename... Types> requires(ind < sizeof...(Types))
-decltype(auto) get(Tuple<Types...>& t) {
-    return static_cast<TupleItems::TupleUnit<ind, typename GetTypes::impl<ind, Types...>::Type>&>(t).get();
-}
 
 int main() {
 
